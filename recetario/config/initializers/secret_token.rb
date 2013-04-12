d3c2daab4986e339fab33585f79b9751281a7e84
FranLudin <franlugaes@hotmail.com>
2013-04-11 19:20:22 -0500

    Upload project

---
+++ recetario/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recetario::Application.config.secret_token = 'dc374061c7f1636ce148f03520bfd97c1938037cff46bec3935f200a0e55dd7c6065b7e7fa1826c400f167bafa901ac08814c70d1a3cd469a0c6cea743d582fa'
